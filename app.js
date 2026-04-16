db34e831d0c91044312251b9c48ead7a79358666
Ankita Kumari <ankitakk615@gmail.com>
2026-04-16 11:49:40 +0530

    Refactor game logic and fix random index generation

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 let gameSeq = [];
 let userSeq = [];
-
 let btns = ["yellow", "red", "green", "blue"];
-
 let started = false;
 let level = 0;
-
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function() {
+document.addEventListener("keypress", function () {
     if (started == false) {
         console.log("Game is Start");
         started = true;
-
         levelUp();
     }
 });
@@ -33,11 +29,10 @@ function gameFlash(btn) {
 
 function levelUp() {
     userSeq = [];
-    level++ ;
+    level++;
     h2.innerText = `Level ${level}`;
 
-    //random btn choose
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
     gameSeq.push(randColor);
@@ -54,7 +49,7 @@ function checkAns(idx) {
         h2.innerHTML = `Game over ! Your score was : <b>${level * 2}</b> <br> Press any key to Restart.`;
         document.querySelector("body").style.backgroundColor = "red";
         setTimeout(function () {
-            document.querySelector("body").style.backgroundColor = "default";
+            document.querySelector("body").style.backgroundColor = "";
         }, 150);
         reset();
     }
@@ -63,15 +58,13 @@ function checkAns(idx) {
 function btnPress() {
     let btn = this;
     userFlash(btn);
-
-    userColor = btn.getAttribute("id");
+    let userColor = btn.getAttribute("id");
     userSeq.push(userColor);
-
-    checkAns(userSeq.length  - 1);
+    checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
-for (btn of allBtns) {
+for (const btn of allBtns) {
     btn.addEventListener("click", btnPress);
 }
 
